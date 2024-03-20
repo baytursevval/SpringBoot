1fbb17e8ef5daac783dae03072a7f6d6fa29e89c
baytursevval <101838447+baytursevval@users.noreply.github.com>
2024-03-20 15:04:05 +0300

    Add files via upload

---
+++ 03-properties-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,29 @@
+package com.luv2code.springboot.demo.mycoolapp.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "coach:" + coachName + "team: "+ teamName;
+    }
+
+    @GetMapping("/")
+    public String sayHello(){
+        return "hello";
+    }
+
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+
+        return "hello 2";
+    }
+}
