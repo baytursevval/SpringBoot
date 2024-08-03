eee04afcb18b10be7348e1979d6c69ad1ba6fb01
baytursevval <baytursevval@gmail.com>
2024-08-03 18:47:50 +0300

    Rest CRUD Demo

---
+++ 09-rest-crud/demo/src/main/java/com/example/demo/rest/StudentRestController.java
@@ -0,0 +1,43 @@
+package com.example.demo.rest;
+
+import com.example.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+    private List<Student> theStudents;
+
+    @PostConstruct //only load student data once
+    public void loadData(){
+        theStudents = new ArrayList<>();
+
+        theStudents.add(new Student("john", "lenn"));
+        theStudents.add(new Student("mary", "rossi"));
+        theStudents.add(new Student("mario", "smith"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+
+        return theStudents;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId){
+
+        if( (studentId>=theStudents.size()) || (studentId < 0) ) {
+            throw new StudentNotFoundException("student id not found: " + studentId);
+        }
+
+        return theStudents.get(studentId); //use studentId for the index
+    }
+
+}
